951e8f3ea6bcae9fa39e0fd65cf0bce72bb3d68d
DicmaryZambrano <dicmaryzambrano@outlook.com>
2023-01-27 23:41:08 -0400

    Created Jorunal App

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,19 +5,19 @@ class Program
     static void Main(string[] args)
     {
         Job job1 = new Job();
-        job1._jobTitle = "Software Engineer";
-        job1._company = "Microsoft";
-        job1._startYear = 2019;
-        job1._endYear = 2022;
+        job1._jobTitle = "Game Developer";
+        job1._company = "PlayStation";
+        job1._startYear = 2002;
+        job1._endYear = 2007;
 
         Job job2 = new Job();
-        job2._jobTitle = "Manager";
-        job2._company = "Apple";
-        job2._startYear = 2022;
-        job2._endYear = 2023;
+        job2._jobTitle = "Concept Artist";
+        job2._company = "Blizard Entretaiment";
+        job2._startYear = 2020;
+        job2._endYear = 2022;
 
         Resume myResume = new Resume();
-        myResume._name = "Allison Rose";
+        myResume._name = "Dicmary Zambrano";
 
         myResume._jobs.Add(job1);
         myResume._jobs.Add(job2);
