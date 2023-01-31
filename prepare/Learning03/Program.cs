b08452b2555f08c8cae0deead8564ec0fc8c4c3e
DicmaryZambrano <dicmaryzambrano@outlook.com>
2023-01-30 22:59:52 -0400

    created code for functions program

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,7 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        /*
+        Fraction firstFraction = new Fraction();
+        Fraction secondFraction = new Fraction(5);
+        Fraction thirdFraction = new Fraction(3,4);
+        Fraction fourtFraction = new Fraction(1,3);
+
+        Console.WriteLine(firstFraction.GetBottom());
+        Console.WriteLine(firstFraction.GetTop());
+        Console.WriteLine(secondFraction.GetBottom());
+        Console.WriteLine(secondFraction.GetTop());
+        Console.WriteLine(thirdFraction.GetBottom());
+        Console.WriteLine(thirdFraction.GetTop());
+        */
+
+        Fraction firstFraction = new Fraction();
+        Fraction secondFraction = new Fraction(5);
+        Fraction thirdFraction = new Fraction(3,4);
+        Fraction fourtFraction = new Fraction(1,3);
+
+        Console.WriteLine(firstFraction.GetFractionString());
+        Console.WriteLine(firstFraction.GetDecimalValue());
+
+        Console.WriteLine(secondFraction.GetFractionString());
+        Console.WriteLine(secondFraction.GetDecimalValue());
+
+        Console.WriteLine(thirdFraction.GetFractionString());
+        Console.WriteLine(thirdFraction.GetDecimalValue());
+
+        Console.WriteLine(fourtFraction.GetFractionString());
+        Console.WriteLine(fourtFraction.GetDecimalValue());
+
     }
 }
 
