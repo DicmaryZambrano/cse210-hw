090e81ca94e819b12dd16866353f817ff55200ae
DicmaryZambrano <dicmaryzambrano@outlook.com>
2023-02-23 22:28:05 -0400

    Created code for mindfullness program

---
+++ prove/Develop04/PromptGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Develop04
+{
+    class PromptGenerator
+    {
+        private List<string> _list;
+        public PromptGenerator(List<string> list)
+        {
+            _list = list;
+        }
+
+        public string CreatePrompt()
+        {
+            Random random = new Random();
+
+            var randomIndex = random.Next(0, _list.Count);
+            string prompt = _list[randomIndex];
+
+            return prompt;   
+        }
+    }
+}
